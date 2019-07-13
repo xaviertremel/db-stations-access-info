adf93e4812f9dca8dbf5b7e52c129fa37241d279
Xavier Tremel <xavier.tremel@gmail.com>
2019-07-13 10:10:48 +0200

    Fetch stations

--- src/Screens/Stations.js
+++ src/Screens/Stations.js
@@ -2,11 +2,35 @@ import React from 'react'
 
 class Stations extends React.Component {
   state = {
-
+    stations: [],
+    filteredStations: [],
+    searchTerm: '',
   }
 
-  render = () => <p>Stations</p>
+  render = () => 
+    <div className="Stations">
+      <input type="text" value={this.state.searchTerm} onChange={(e) => this.handleChange(e.target.value)} />
+      <div className="StationsList">
+        {this.state.filteredStations.map(station => <p>{station.name}</p>)}
+      </div>
+    </div>
 
+  componentDidMount = () => {
+    fetch('https://api.deutschebahn.com/stada/v2/stations', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer 584de6f81a61906e18ed0d2602742f09'
+      }
+    })
+      .then(res => res.json())
+      .then(({ result }) => this.setState({ stations: result, filteredStations: result }))
+      .catch(error => console.error('Error:', error));
+  }
+
+  handleSearch = (searchTerm) => {
+    this.setState({ searchTerm })
+  }
 }
 
 export default Stations
